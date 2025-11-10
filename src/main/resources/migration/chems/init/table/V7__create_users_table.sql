b5b171c4a33fe17a763e117ae85bde7620967f23
droplessrain <aleks.gavrila@mail.ru>
2025-11-10 18:44:22 +0300

    replace verification to check

--- src/main/resources/migration/chems/init/table/V7__create_users_table.sql
+++ src/main/resources/migration/chems/init/table/V7__create_users_table.sql
@@ -1,21 +1,8 @@
-CREATE OR REPLACE FUNCTION check_email_regex(p_email VARCHAR)
-RETURNS BOOLEAN AS $$
-BEGIN
-    IF p_email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$' THEN
-        RETURN TRUE;
-ELSE
-        RETURN FALSE;
-END IF;
-END;
-$$ LANGUAGE plpgsql IMMUTABLE STRICT;
-
-CREATE DOMAIN email AS VARCHAR(255) CHECK (check_email_regex(VALUE));
-
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
-    user_email EMAIL NOT NULL,
+    email VARCHAR(255) NOT NULL CHECK(email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
     role user_role,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
