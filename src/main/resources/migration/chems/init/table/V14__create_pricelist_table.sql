ce294882d4daa55239044a17499a8b602e2937cb
droplessrain <aleks.gavrila@mail.ru>
2025-11-12 15:48:59 +0300

    rename enum service-type

--- src/main/resources/migration/chems/init/table/V14__create_pricelist_table.sql
+++ src/main/resources/migration/chems/init/table/V14__create_pricelist_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS pricelist (
     id BIGSERIAL PRIMARY KEY,
     contractor_id BIGINT NOT NULL,
-    service_type SERVICE_TYPE NOT NULL,
+    service_type service_type NOT NULL,
     quantity_of_hours SMALLINT NOT NULL CHECK (quantity_of_hours > 0),
     price BIGINT NOT NULL CHECK (price > 0),
-    experience VARCHAR(400) NOT NULL,
+    experience TEXT NOT NULL,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_pricelist_contractor FOREIGN KEY (contractor_id) REFERENCES contractor(id)
