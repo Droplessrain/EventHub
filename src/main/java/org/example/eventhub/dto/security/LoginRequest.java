b0ea5bfbe2ee1059d20035a3906baa6267ff1504
droplessrain <aleks.gavrila@mail.ru>
2025-11-28 16:24:11 +0300

    check length in db

--- src/main/java/org/example/eventhub/dto/security/LoginRequest.java
+++ src/main/java/org/example/eventhub/dto/security/LoginRequest.java
@@ -6,9 +6,9 @@ import jakarta.validation.constraints.Size;
 public record LoginRequest(
 
         @NotNull(message = "username is required")
-        @Size(min = 8, max = 20, message = "username must be between 8 and 20")
+        @Size(min = 8, max = 50, message = "username must be between 8 and 50")
         String username,
 
         @NotNull(message = "password is required")
-        @Size(min = 8, max = 20, message = "password must be between 8 and 20")
+        @Size(min = 8, max = 100, message = "password must be between 8 and 100")
         String password) {}
--- src/main/resources/migration/chems/init/table/V07__create_users_table.sql
+++ src/main/resources/migration/chems/init/table/V07__create_users_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL CHECK(email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    username VARCHAR(50) NOT NULL UNIQUE CHECK(length(username) > 8),
+    password VARCHAR(100) NOT NULL CHECK(length(password) > 8),
+    email VARCHAR(255) NOT NULL UNIQUE CHECK(email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
     user_role user_role NOT NULL,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
